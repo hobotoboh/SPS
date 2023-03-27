fda8c60d2d3168543feeba7192ed6bc5cf452629
hobotoboh <hbtboy19@gmail.com>
2023-03-27 23:56:09 +0300

    well, that's still a start

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press Ctrl+F8 to toggle the breakpoint.
+
+from tkinter import *
+
+root = Tk()
+root.title("Автоматизированная система")
+root.geometry("1000x600+200+50")
+root.resizable(width=False, height=False)
+
+root.mainloop()
