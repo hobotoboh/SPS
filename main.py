c87700be09258679c3398c93d453c39f13cd9a32
hobotoboh <hbtboy19@gmail.com>
2023-04-07 01:13:26 +0300

    app

--- main.py
+++ main.py
@@ -3,11 +3,46 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # Press Ctrl+F8 to toggle the breakpoint.
 
-from tkinter import *
+# from tkinter import *
+import plotly.express as px
+import pandas as pd
+import sqlite3
 
-root = Tk()
-root.title("Автоматизированная система")
-root.geometry("1000x600+200+50")
-root.resizable(width=False, height=False)
+print('Получение данных...')
+filename = r"A:\Files\Diploma\AIS_10_01_2021.db"
+sql = (
+    "SELECT ais.mmsi, ais.Longitude, ais.Latitude, Vessels.referencePointA "
+    "FROM ais, Vessels "
+    "WHERE ais.mmsi = Vessels.mmsi"
+)
 
-root.mainloop()
+try:
+    cnn = sqlite3.connect(filename)
+    dataframe = pd.read_sql(sql, cnn)
+    print(dataframe.head(20))
+    figure = px.scatter_mapbox(dataframe,
+                               lon=dataframe['Longitude'],
+                               lan=dataframe['Latitude'],
+                               zoom=5,
+                               size=dataframe['referencePointA'],
+                               width=900,
+                               height=600,
+                               title="AIS 2021")
+    figure.update_layout(mapbox_style="open-street-map")
+    figure.show()
+    print("График построен")
+
+
+except Exception as exc:
+    print(exc)
+finally:
+    if cnn:
+        cnn.close()
+        print('Соединение прекращено')
+
+# root = Tk()
+# root.title("Автоматизированная система")
+# root.geometry("1000x600+200+50")
+# root.resizable(width=False, height=False)
+#
+# root.mainloop()
